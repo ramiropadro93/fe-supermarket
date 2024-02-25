5579baefea650903418d9a3f7fad29e4e9b3c5e0
Ramiro Padró <ramipadro93@gmail.com>
2024-02-25 17:37:13 -0300

    prueba

--- pages/productsTable.js
+++ pages/productsTable.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './ProductsTable.css';
 import { API_URL } from './constants';
 
 const ProductsTable = () => {
