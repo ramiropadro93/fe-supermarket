6f31e6b1c77f34c1f7eaa34d9b3c276001949549
Ramiro Padró <ramipadro93@gmail.com>
2024-02-25 17:50:26 -0300

    prueba

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import ProductsTable from './productsTable'
+import ProductsTable from '../components/productsTable'
 
 export default function Home() {
   return (
